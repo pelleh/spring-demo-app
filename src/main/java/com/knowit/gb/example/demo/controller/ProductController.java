0fb90e21e68a7a74ead438f0802ecad98d73a32a
Per Hedblom <pelle.hedblom@yahoo.com>
2023-08-24 14:25:01 +0200

    Added methods for product

--- src/main/java/com/knowit/gb/example/demo/controller/ProductController.java
+++ src/main/java/com/knowit/gb/example/demo/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,8 +21,23 @@ public class ProductController {
 		this.productRepository =productRepository;
 	}
 
-	@GetMapping
+	@GetMapping("/")
 	public ResponseEntity<List<Product>> getAllProducts() {
 		return ResponseEntity.ok(productRepository.findAll());
+	}
+
+	@GetMapping("/name/{name}")
+	public ResponseEntity<List<Product>> findProductByName(@PathVariable String name) {
+		return ResponseEntity.ok(productRepository.findByNameContains(name));
+	}
+
+	@GetMapping("/desc/{desc}")
+	public ResponseEntity<List<Product>> findProductByDesc(@PathVariable String desc) {
+		return ResponseEntity.ok(productRepository.findByDescriptionContains(desc));
+	}
+
+	@GetMapping("/description/{desc}")
+	public ResponseEntity<List<Product>> findProductByDescription(@PathVariable String desc) {
+		return ResponseEntity.ok(productRepository.findByDescriptionContains(desc));
 	}
 }
--- src/main/java/com/knowit/gb/example/demo/repository/ProductRepository.java
+++ src/main/java/com/knowit/gb/example/demo/repository/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.knowit.gb.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.knowit.gb.example.demo.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	List<Product> findByNameContains(String name);
+
+	List<Product> findByDescriptionContains(String name);
 
 }
